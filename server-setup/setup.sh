6ba828a80c12797f57653d348ac49e637202ea20
Nipun Garg <nipun.garg@flipkart.com>
2021-05-16 01:14:31 +0530

    nginx conf

--- server-setup/setup.sh
+++ server-setup/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-cp  server-setup/care2donate_server_supervisor.conf /etc/supervisor/conf.d/care2donate.conf
+cp server-setup/care2donate_server_supervisor.conf /etc/supervisor/conf.d/care2donate.conf
+cp server-setup/nginx.conf /etc/nginx/sites-available/care2donate
