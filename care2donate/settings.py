71b23d51166084397a1b24a46d954ce5f6c62cf2
Nipun Garg <nipun.garg@flipkart.com>
2021-05-16 00:52:39 +0530

    Updates allowed host

--- care2donate/settings.py
+++ care2donate/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-53@e704=hp*@rw96#=51u8f@q!+pdr3m!n284#s_^_ouw@tf@3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
