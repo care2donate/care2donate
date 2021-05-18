7c48410a799d72d36d50c37cfb00c8f63c8ab64a
arjun gupta <arjuntheprogrammer@gmail.com>
2021-05-19 02:19:16 +0530

    updated input forms

--- covid/forms.py
+++ covid/forms.py
@@ -1,23 +1,54 @@
-from django import forms
+from datetime import datetime
+from django.forms import ModelForm, DateField, SelectDateWidget
+
 
 from covid.models import CovidUserDetail
 from user.models import User
 
 
-class CovidUserForm(forms.ModelForm):
+class CovidUserForm(ModelForm):
+    YEARS = [x for x in range(2019, 2023)]
+
+    dob = DateField(
+        widget=SelectDateWidget(
+            years=YEARS,
+            empty_label=("Choose Year", "Choose Month", "Choose Day"),
+        ),
+        initial=datetime.now()
+    )
+
     class Meta:
         model = User
         fields = (
-            'name', 'gender', 'dob',
+            'name', 'gender',
+            'dob',
             'address', 'city',
             'contact_number'
         )
 
 
-class CovidUserDetailForm(forms.ModelForm):
+class CovidUserDetailForm(ModelForm):
+    YEARS = [x for x in range(2019, 2023)]
+
+    covid_positive_date = DateField(
+        widget=SelectDateWidget(
+            years=YEARS,
+            empty_label=("Choose Year", "Choose Month", "Choose Day"),
+        ),
+        initial=datetime.now()
+    )
+    covid_negate_date = DateField(
+        widget=SelectDateWidget(
+            years=YEARS,
+            empty_label=("Choose Year", "Choose Month", "Choose Day"),
+        ),
+        initial=datetime.now()
+    )
+
     class Meta:
         model = CovidUserDetail
         fields = (
-            'user_type', 'blood_group', 'covid_positive_date',
-            'covid_negate_date', 'remarks',
+            'user_type', 'blood_group',
+            'covid_positive_date', 'covid_negate_date',
+            'remarks',
         )
